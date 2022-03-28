d414884afb6954ad13affa8a7d5b8b2391ec95ef
Ray <rayw.public@gmail.com>
2022-03-28 13:43:35 +0800

    shorten the summary

--- translate-mode.el
+++ translate-mode.el
@@ -1,4 +1,4 @@
-;;; translate-mode.el --- Paragraph-oriented minor mode for side-by-side document translation workflow -*- lexical-binding: t; -*-
+;;; translate-mode.el --- Paragraph-oriented side-by-side document translation workflow -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2022 Ray Wang <rayw.public@gmail.com>
 
